77aec98bdec50a6af03df928286b932d39526cb3
LadyHail <ladydeath@o2.pl>
2018-05-24 13:29:08 +0200

    Init commit

---
+++ src/Container/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <p>aa</p>
+    );
+  }
+}
+
+export default App;
